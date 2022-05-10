a339f0a234fc11fcce602c075d0b2baab9c03185
керимов алихан <yakhya2809@gmail.com>
2022-05-10 13:56:24 +0300

    news_main

---
+++ index.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const express = require('express')
+
+const app = express()
+
+const port = 4000
+
+app.use(express.json())
+
+
+mongoose.connect('mongodb+srv://Isa:28092002@cluster0.fnkei.mongodb.net/news_team').then(() => console.log('Успешно соединились с сервером MongoDB')).catch(() => console.log('Ошибка при соединении с сервером MongoDB'))
+
+app.listen(port, () => {
+  console.log(`hello port http://localhost:${port}`)
+})
